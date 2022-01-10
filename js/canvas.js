803380ff09def821a448f42a14002db77a21a38c
Gabriel Lee <gelos@unknown845cf33cbfb3.attlocal.net>
2022-01-10 15:28:20 -0500

    Improved style consistency by converting all snake case to camel case

--- js/canvas.js
+++ js/canvas.js
@@ -43,69 +43,69 @@ function onMouseUp() {
 function updateDimensions() {
   var width = window.canvas.getBoundingClientRect().width,
       height = window.canvas.getBoundingClientRect().height;
-  window.wh_ratio = width / height;
+  window.whRatio = width / height;
 
   var lo = 0,
       hi = window.ratios.length - 1,
       mid;
   while (hi > lo + 1) {
     mid = Math.floor((hi + lo) / 2);
-    if (window.wh_ratio > window.ratios[mid].ratio) lo = mid;
+    if (window.whRatio > window.ratios[mid].ratio) lo = mid;
     else hi = mid;
   }
 
   var ratio;
-  if (window.wh_ratio - window.ratios[lo].ratio < 
-      window.ratios[hi].ratio - window.wh_ratio) ratio = window.ratios[lo];
+  if (window.whRatio - window.ratios[lo].ratio < 
+      window.ratios[hi].ratio - window.whRatio) ratio = window.ratios[lo];
   else ratio = window.ratios[hi];
 
   window.width = ratio.width;
   window.height = ratio.height;
   window.canvas.width = width;
   window.canvas.height = height;
-  window.tile_dim = Math.min(
+  window.tileDim = Math.min(
     width / (window.width + 1),
     height / (window.height + 1)
   );
-  window.offset_x = (width - (window.width * window.tile_dim)) / 2;
-  window.offset_y = (height - (window.height * window.tile_dim)) / 2;
+  window.offsetX = (width - (window.width * window.tileDim)) / 2;
+  window.offsetY = (height - (window.height * window.tileDim)) / 2;
 
   for (var i = 0; i < 48; i++)
-    render_tile(i);
+    renderTile(i);
 }
 
-function render_tile(index) {
-  var x = (index % window.width) * window.tile_dim + window.offset_x,
-      y = Math.floor(index / window.width) * window.tile_dim + window.offset_y,
-      r = window.tile_dim / 20,
-      margin = window.tile_dim / 50;
+function renderTile(index) {
+  var x = (index % window.width) * window.tileDim + window.offsetX,
+      y = Math.floor(index / window.width) * window.tileDim + window.offsetY,
+      r = window.tileDim / 20,
+      margin = window.tileDim / 50;
 
   window.ctx.beginPath();
   window.ctx.strokeStyle="black";
   window.ctx.moveTo(x + r + margin, y + margin);
-  window.ctx.lineTo(x + window.tile_dim - r - margin, y + margin);
+  window.ctx.lineTo(x + window.tileDim - r - margin, y + margin);
   window.ctx.quadraticCurveTo(
-    x + window.tile_dim - margin,
+    x + window.tileDim - margin,
     y + margin,
-    x + window.tile_dim - margin,
+    x + window.tileDim - margin,
     y + r + margin
   );
   window.ctx.lineTo(
-    x + window.tile_dim - margin,
-    y + window.tile_dim - r - margin
+    x + window.tileDim - margin,
+    y + window.tileDim - r - margin
   );
   window.ctx.quadraticCurveTo(
-    x + window.tile_dim - margin,
-    y + window.tile_dim - margin,
-    x + window.tile_dim - r - margin,
-    y + window.tile_dim - margin
+    x + window.tileDim - margin,
+    y + window.tileDim - margin,
+    x + window.tileDim - r - margin,
+    y + window.tileDim - margin
   );
-  window.ctx.lineTo(x + r + margin, y + window.tile_dim - margin);
+  window.ctx.lineTo(x + r + margin, y + window.tileDim - margin);
   window.ctx.quadraticCurveTo(
     x + margin,
-    y + window.tile_dim - margin,
+    y + window.tileDim - margin,
     x + margin,
-    y + window.tile_dim - r - margin
+    y + window.tileDim - r - margin
   );
   window.ctx.lineTo(x + margin, y + r + margin);
   window.ctx.quadraticCurveTo(x + margin, y + margin, x + r + margin, y + margin);
